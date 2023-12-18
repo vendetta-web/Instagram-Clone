52bbc841fcdd5c13840c3e4e1fd54bd248810ce6
Vendetta-Web <sk564228@gmail.com>
2023-12-18 11:42:18 +0530

    added the footer copyright on auth page

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -3,43 +3,75 @@ import { useState } from "react";
 import Login from "./Login";
 import Signup from "./Signup";
 import GoogleAuth from "./GoogleAuth";
+import { Link } from "react-router-dom";
+
+const currentYear = new Date().getFullYear();
 
 const AuthForm = () => {
-	const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(true);
 
-	return (
-		<>
-			<Box border={"1px solid gray"} borderRadius={4} padding={5}>
-				<VStack spacing={4}>
-					<Image src='/logo.png' h={24} cursor={"pointer"} alt='Instagram' />
+  return (
+    <>
+      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <VStack spacing={4}>
+          <Image src="/logo.png" h={24} cursor={"pointer"} alt="Playhacks" />
 
-					{isLogin ? <Login /> : <Signup />}
+          {isLogin ? <Login /> : <Signup />}
 
-					{/* ---------------- OR -------------- */}
-					<Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"}>
-						<Box flex={2} h={"1px"} bg={"gray.400"} />
-						<Text mx={1} color={"white"}>
-							OR
-						</Text>
-						<Box flex={2} h={"1px"} bg={"gray.400"} />
-					</Flex>
+          {/* ---------------- OR -------------- */}
+          <Flex
+            alignItems={"center"}
+            justifyContent={"center"}
+            my={4}
+            gap={1}
+            w={"full"}
+          >
+            <Box flex={2} h={"1px"} bg={"gray.400"} />
+            <Text mx={1} color={"white"}>
+              OR
+            </Text>
+            <Box flex={2} h={"1px"} bg={"gray.400"} />
+          </Flex>
 
-					<GoogleAuth prefix={isLogin ? "Log in" : "Sign up"} />
-				</VStack>
-			</Box>
+          <GoogleAuth prefix={isLogin ? "Log in" : "Sign up"} />
+        </VStack>
+      </Box>
 
-			<Box border={"1px solid gray"} borderRadius={4} padding={5}>
-				<Flex alignItems={"center"} justifyContent={"center"}>
-					<Box mx={2} fontSize={14}>
-						{isLogin ? "Don't have an account?" : "Already have an account?"}
-					</Box>
-					<Box onClick={() => setIsLogin(!isLogin)} color={"blue.500"} cursor={"pointer"}>
-						{isLogin ? "Sign up" : "Log in"}
-					</Box>
-				</Flex>
-			</Box>
-		</>
-	);
+      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <Flex alignItems={"center"} justifyContent={"center"}>
+          <Box mx={2} fontSize={14}>
+            {isLogin ? "Don't have an account?" : "Already have an account?"}
+          </Box>
+          <Box
+            onClick={() => setIsLogin(!isLogin)}
+            color={"blue.500"}
+            cursor={"pointer"}
+          >
+            {isLogin ? "Sign up" : "Log in"}
+          </Box>
+        </Flex>
+      </Box>
+      <Flex
+        fontSize={12}
+        color={"gray.300"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        my={4}
+        gap={1}
+        w={"full"}
+      >
+        © {currentYear} Built By{" "}
+        <Link
+          href="https://saurabhdixit.vercel.app"
+          target="_blank"
+          color="blue.500"
+          fontSize={14}
+        >
+          Vendetta-Web
+        </Link>
+      </Flex>
+    </>
+  );
 };
 
 export default AuthForm;
